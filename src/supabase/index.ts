2f6167f61f85b2a36613af929bfc3412db1fdfc4
DQuinn97 <quinten2407@gmail.com>
2025-06-02 12:56:29 +0200

    _

--- src/supabase/index.ts
+++ src/supabase/index.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./types";
-const supabaseUrl = "https://jegwxnhpinsvialqonbr.supabase.co";
-const supabaseKey = process.env.NEXT_SUPABASE_KEY as string;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export default supabase;
